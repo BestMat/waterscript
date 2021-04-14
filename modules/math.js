e72c8328a3c9755125be673351798efeb959e18a
BestMat <76582849+BestMat@users.noreply.github.com>
2021-04-14 14:54:00 +0530

    Create math.js

---
+++ modules/math.js
@@ -0,0 +1,18 @@
+export function round(a){
+    return Math.round(a);
+}
+export function ceil(a){
+    return Math.ceil(a);
+}
+export function floor(a){
+    return Math.floor(a);
+}
+export function trunc(a){
+    return Math.trunc(a);
+}
+export function power(x,y){
+    return Math.pow(x,y);
+}
+export function randomNumber(x){
+    return Math.floor((Math.random() * x) + 1);
+}
