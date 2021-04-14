80ee4ccfb235e62f9aa22fd4302ce3895a5c4676
BestMat <76582849+BestMat@users.noreply.github.com>
2021-04-14 14:54:31 +0530

    Create date.js

---
+++ src/date.js
@@ -0,0 +1,19 @@
+var date = new Date();
+export function getDate(){
+    return date.getDate();
+}
+export function getYear(){
+    return date.getFullYear();
+}
+export function getMinutes(){
+    return date.getMinutes();
+}
+export function getHours(){
+    return date.getHours();  
+}
+export function getSeconds(){
+    return date.getSeconds();  
+}
+export function getMilliseconds(){
+    return date.getMilliseconds();  
+}
